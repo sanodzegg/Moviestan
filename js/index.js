953e44d87e18966ab006a3686f56d5bca594922f
unknown <sanodzegg@gmail.com>
2022-01-31 12:53:13 +0400

    navigation update/added tv-shows page

--- js/index.js
+++ js/index.js
@@ -3,16 +3,31 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 const MOST_POPULAR = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
 const POSTER_URL = 'https://image.tmdb.org/t/p/w500';
 const BACKDROP_URL = 'https://image.tmdb.org/t/p/original';
-const THEATRE_URL = BASE_URL + '/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2021-10-22&' + API_KEY;
+const THEATRE_URL = BASE_URL + `/discover/movie?primary_release_date.gte=2020-01-01&primary_release_date.lte=${currentDate()}&` + API_KEY;
 
 $(document).ready(function(){
     $('.main-loader').fadeOut("slow");
-    randomMovie();
+    let content = {
+        0: {
+            loader : $('.main-loader').prop('outerHTML'),
+            mainSect : $('.main-section').html(),
+            moviescroll : $('.movie-list-section').html()
+        },
+        1: {
+            
+        },
+        2: {
+            nav : 'sec'
+        },
+        3: {
+            nav : 'last'
+        }
+    }
+    getMovies(MOST_POPULAR)
     randomScroll();
     // contentDrag();
     setTimeout(() => {
         if($('.description-par').text().length > 350) {
-            // $('.description-par').addClass('fade')
             $('.description-par').addClass('overflow-scroll')
         }
     },100)
@@ -34,8 +49,49 @@ $(document).ready(function(){
             $('.search-results').text('')
         }
     })
+
+    // navigation
+    function navigation() {
+        let nav = $('li');
+        for(let i = 0; i < nav.length; i++) {
+            nav[i].addEventListener('click', function(){
+                if(i == 0) {
+                    $('body').append(`${content[0].loader}`);
+                    $('.main-loader').fadeOut("slow");
+                    $('.main-section').html(`${content[0].mainSect}`);
+                    $('.movie-list-section').html(`${content[0].moviescroll}`);
+                    getMovies(MOST_POPULAR)
+                    randomScroll();
+                    setTimeout(() => {
+                        if($('.description-par').text().length > 350) {
+                            $('.description-par').addClass('overflow-scroll');
+                        }
+                    },100)
+                } else {
+                    $('.main-loader').remove();
+                    $('body').css('background', 'none');
+                    $('section').html('');
+                }
+                if(i == 1) {
+                    $('body').css({
+                        'background':'#0F101A',
+                        'overflow-y':'scroll'
+                    });
+                    $('body').append('<section role="Film poster and description" class="shows-main-grid"></section>')
+                    getShows(1); // must be greater than 0;
+                    var x = 0;
+                    $('body').scroll(function() {
+                        console.log(x);
+                    })
+                } else {
+                    $('.shows-main-grid').remove();
+                }
+            })
+        }
+    }
     navigation();
 })
+
 
 
 function getConf() {
@@ -60,13 +116,12 @@ getGenres()
 
 function getMovies(url) {
     fetch(url).then(res => res.json()).then(data => {
-        randomMovie(data);
+        randomMovie(data.results);
         $('.watch').on('click', function(){
             getVideo(data)
         })
     })
 }
-getMovies(MOST_POPULAR)
 
 let val = new Number;
 
@@ -74,19 +129,19 @@ function randomMovie(data) {
     try {
         val = getRandom(0, 20)
         $('body').css({
-            'background' : `radial-gradient(circle, rgba(0,0,0,0) 0%, rgba(0,0,0,1) 100%), url(${BACKDROP_URL + data.results[val].backdrop_path})`,
+            'background' : `radial-gradient(circle, rgba(0,0,0,0) 0%, rgba(0,0,0,1) 100%), url(${BACKDROP_URL + data[val].backdrop_path})`,
             'background-position' : 'center',
             'background-repeat' : 'no-repeat',
             'background-size' : 'cover'
         })
-        $('.header').text(data.results[val].original_title)
-        $('.stars').attr('style',`--rating: ${data.results[val].vote_average/2};`);
-        for(let i = 0; i < data.results[val].genre_ids.length; i++) {
+        $('.header').text(data[val].original_title)
+        $('.stars').attr('style',`--rating: ${data[val].vote_average/2};`);
+        for(let i = 0; i < data[val].genre_ids.length; i++) {
             $('.genres').append(`<span>${obj[i].name}</span>`)
         }
-        $('.description-par').text(data.results[val].overview)
+        $('.description-par').text(data[val].overview)
     } catch (err) {
-        
+        // console.error(err);
     }
 }
 
@@ -155,17 +210,21 @@ function getVideo(data){
     fetch(`${BASE_URL}/movie/${data.results[val].id}/videos?${API_KEY}`).then(res => res.json())
     .then(data => {
        if(data){
-           var embed = [];
-           data.results.forEach(v => {
-               let {name, key, site, type} = v;
-               if(site == 'YouTube' && type == 'Trailer') {
-                   embed.push(`
-                   <iframe width="560" height="315" src="https://www.youtube.com/embed/${key}" 
-                   title="${name}" frameborder="0" allow="accelerometer; autoplay; 
-                   clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                   `)
-               }
-           })
+        try {
+            var embed = [];
+            data.results.forEach(v => {
+                let {name, key, site, type} = v;
+                if(site == 'YouTube' && type == 'Trailer') {
+                    embed.push(`
+                    <iframe src="https://www.youtube.com/embed/${key}" 
+                    title="${name}" frameborder="0" allow="accelerometer; autoplay; 
+                    clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    `)
+                }
+            })
+        } catch(err) {
+            console.log(err);
+        }
        }
        $('body').append(`<div class="background-shadow">
        <div class="embed-loader"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 11c.511-6.158 5.685-11 12-11s11.489 4.842 12 11h-2.009c-.506-5.046-4.793-9-9.991-9s-9.485 3.954-9.991 9h-2.009zm21.991 2c-.506 5.046-4.793 9-9.991 9s-9.485-3.954-9.991-9h-2.009c.511 6.158 5.685 11 12 11s11.489-4.842 12-11h-2.009z"/></svg></div>
@@ -217,10 +276,41 @@ function search(){
     });
 }
 
-function navigation() {
-
+async function getShows(page) {
+    let res = await fetch(`${BASE_URL}/tv/popular?${API_KEY}&page=${page}`)
+    let data = await res.json();
+    console.log(data);
+    data.results.forEach(show => {
+        let {name, backdrop_path} = show;
+        $('.shows-main-grid').append(`
+            <div class="show-block">
+                <div class="img-container">
+                    <div class="img-loader"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 11c.511-6.158 5.685-11 12-11s11.489 4.842 12 11h-2.009c-.506-5.046-4.793-9-9.991-9s-9.485 3.954-9.991 9h-2.009zm21.991 2c-.506 5.046-4.793 9-9.991 9s-9.485-3.954-9.991-9h-2.009c.511 6.158 5.685 11 12 11s11.489-4.842 12-11h-2.009z"/></svg></div>
+                    <div class="img-container-shade"></div>
+                    <img src="${'https://image.tmdb.org/t/p/w300'+ backdrop_path}">
+                </div>
+                <div class="title-space">
+                <h6>${name}</h6>
+                <div class="settings">•••</div>
+            </div>
+            </div>
+        `)
+        $('img').ready(function(){
+            $('.img-loader').fadeOut('slow')
+        })
+    });
 }
+
 
 function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
+}
+
+function currentDate(date) {
+    date = new Date();
+    let dd = String(date.getDate()).padStart(2, '0');
+    let mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = date.getFullYear();
+    date = `${yyyy}-${mm}-${dd}`;
+    return date
 }
